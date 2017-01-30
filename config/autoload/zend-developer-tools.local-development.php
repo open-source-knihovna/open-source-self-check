e463a4ca277c49ccf85aa58cbc7130e9f0f0304f
Josef Moravec <josef.moravec@gmail.com>
2017-01-30 09:43:45 +0100

    Update copyright and license information

--- config/autoload/zend-developer-tools.local-development.php
+++ config/autoload/zend-developer-tools.local-development.php
@@ -1,8 +1,7 @@
 <?php
 /**
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @copyright Copyright (c) 2016 Tohu development team
+ * @license   https://www.gnu.org/licenses/gpl.html GNU GENERAL PUBLIC LICENSE Version 3
  */
 
 /**
