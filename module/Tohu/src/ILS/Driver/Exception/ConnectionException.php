b63000dc02e55ab6fd99e134279a6bf06c2dd38d
Josef Moravec <josef.moravec@gmail.com>
2018-11-06 20:20:36 +0100

    Add ILS Driver exception class

---
+++ module/Tohu/src/ILS/Driver/Exception/ConnectionException.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @copyright Copyright (c) 2016 Tohu development team
+ * @license   https://www.gnu.org/licenses/gpl.html GNU GENERAL PUBLIC LICENSE Version 3
+ */
+
+namespace Tohu\ILS\Driver\Exception;
+
+class ConnectionException extends \Exception
+{
+}
