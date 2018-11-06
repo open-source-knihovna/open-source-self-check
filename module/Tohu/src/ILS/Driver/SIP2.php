b749cb2c9b9167298c2a504f7a9f3035cd0732ab
Josef Moravec <josef.moravec@gmail.com>
2018-11-06 20:31:52 +0100

    Start implementing checkin module

--- module/Tohu/src/ILS/Driver/SIP2.php
+++ module/Tohu/src/ILS/Driver/SIP2.php
@@ -7,6 +7,8 @@
 namespace Tohu\ILS\Driver;
 
 use \sip2 as Sip2Connector;
+use \SIP2\Connection as sip2connection;
+use \Tohu\ILS\Driver\Exception\ConnectionException;
 
 class SIP2 extends AbstractDriver
 {
@@ -15,48 +17,48 @@ class SIP2 extends AbstractDriver
      */
     protected $connection;
 
-    public function init(array $config = [])
+    /*
+     * @var Date format for display
+     */
+    protected $date_format;
+
+    public function init(array $ilsConfig = [])
     {
-        $this->connection = new Sip2Connector();
-        $this->connection->hostname = $config["ils"]["server"] ?? null;
-        $this->connection->port = $config["ils"]["port"] ?? null;
+        $this->date_format = "j. n. Y";
+        $this->connection = new sip2connection(
+            $ilsConfig["server"] ?? null,
+            $ilsConfig["port"] ?? null,
+            $ilsConfig["login"] ?? '',
+            $ilsConfig["password"] ?? ''
+        );
     }
 
-    protected function connect($patronBarcode)
+    public function getPatron($patronBarcode)
     {
-        $this->connection->patron = $patron;
-        $connectResponse = $this->connection->connect();
-        if (!$connectResponse) {
-            throw new \Exception("Can't connect to SIP2 server. Are the hostname and port set properly?");
+        $patron = $this->connection->getPatronInfo($patronBarcode, true);
+        $checkouts = [];
+        foreach ($patron['checkouts'] as $checkout) {
+            $checkouts[] = [
+                'title' => $checkout->variable['AJ'][0] ?? '',
+                'item_location' => $checkout->variable['AQ'][0] ?? '',
+                'checkout_location' => $checkout->variable['BG'][0] ?? '',
+                'barcode' => $checkout->variable['AB'][0] ?? '',
+                'due_date' => isset($checkout->variable['AH'][0]) ?
+                    $this->dateFromSip($checkout->variable['AH'][0]) : '',
+                'circulation_status' => isset($checkout->fixed['CirculationStatus']) ?
+                    $this->getCirculationStatus($checkout->fixed['CirculationStatus']) : '',
+            ];
         }
-        $loginResponse = $this->connection->msgLogin($this->config->login, $this->config->password);
-        /* TODO: some error handling? */
-        $this->connection->parseLoginResponse($this->connection->get_message($loginResponse));
-    }
-
-    protected function getCurrentPatronInfo()
-    {
-        $patronResponse = $this->connection->msgPatronInformation('charged'); //get checkout count again
-        $patron = $this->connection->parsePatronInfoResponse($this->connection->get_message($patronResponse));
         return [
-            'barcode' => $patronBarcode,
-            'name' => $patron['variable']['AE'][0] ?? '',
-            'email' => $patron['variable']['BE'][0] ?? '',
-            'overdues' => $patron['fixed']['OverdueCount'] ?? 0,
-            'holds' => $patron['fixed']['HoldCount'] ?? 0,
-            'checkouts' => $patron['fixed']['ChargedCount'] ?? 0,
-            'fines' => $patron['variable']['BV'][0] ?? 0,
-            'blocked' => (strpos($patron['fixed']['PatronStatus'], 'Y') === false) ? false : true,
-        ];
-    }
-
-    protected function getItemInfo($barcode)
-    {
-        $itemResponse = $this->connection->msgItemInformation($barcode);
-        $item = $this->connection->parseItemInfoResponse($this->connection->get_message($itemResponse));
-        return [
-            'barcode' => $barcode,
-            'id' => $item['variable']['AB'][0] ?? '',
+            'barcode' => $patron['patron']->variable['AA'][0] ?? '',
+            'name' => $patron['patron']->variable['AE'][0] ?? '',
+            'email' => $patron['patron']->variable['BE'][0] ?? '',
+            'overdues' => $patron['patron']->fixed['OverdueCount'] ?? 0,
+            'holds' => $patron['patron']->fixed['HoldCount'] ?? 0,
+            'checkouts' => $patron['patron']->fixed['ChargedCount'] ?? 0,
+            'fines' => $patron['patron']->variable['BV'][0] ?? 0,
+            'blocked' => (strpos($patron['patron']->fixed['PatronStatus'], 'Y') === false) ? false : true,
+            'checkouts' => $checkouts,
         ];
     }
 
@@ -97,11 +99,29 @@ class SIP2 extends AbstractDriver
         ];
     }
 
-    public function getPatron($patronBarcode)
+    protected function dateFromSip($sip_date)
     {
-        $this->connect($patronBarcode);
-        $patron = $this->getCurrentPatronInfo();
-        $this->connection->msgEndPatronSession();
-        return $patron;
+        $date = \DateTime::createFromFormat("Ymd", substr($sip_date, 0, 8));
+        return $date->format($this->date_format);
+    }
+
+    protected function getCirculationStatus($status_number)
+    {
+        switch ($status_number) {
+            case 1: return "other";
+            case 2: return "on order";
+            case 3: return "available ";
+            case 4: return "charged ";
+            case 5: return "charged; not to be recalled until earliest recall date";
+            case 6: return "in process";
+            case 7: return "recalled";
+            case 8: return "waiting on hold shelf";
+            case 9: return "waiting to be re-shelved";
+            case 10: return "in transit between library locations";
+            case 11: return "claimed returned";
+            case 12: return "lost";
+            case 13: return "missing";
+            default: return "unknown";
+        }
     }
 }
