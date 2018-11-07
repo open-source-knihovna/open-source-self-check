7ca9d001767609770af43edf38a8c1b63df97051
Josef Moravec <josef.moravec@gmail.com>
2018-11-07 15:58:52 +0100

    Update language script

--- language.sh
+++ language.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-langs="en_GB cs_CZ"
+srclang="en_GB"
+langs="cs_CZ"
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 cd $dir
 
 tx pull
 
+xgettext --language=php --add-location --output="$dir/module/Tohu/language/$srclang.po" --from-code=UTF-8 --keyword=translate $(find "$dir/" -name '*.phtml') $(find "$dir/module/" -name '*.php')
+
 for lang in $langs
 do
-    xgettext --language=php --add-location --join-existing --output="$dir/module/Tohu/language/$lang.po" --from-code=UTF-8 --keyword=translate $(find "$dir/" -name '*.phtml') $(find "$dir/module/" -name '*.php')
     msgfmt "$dir/module/Tohu/language/$lang.po" -o "$dir/module/Tohu/language/$lang.mo"
 done;
 
