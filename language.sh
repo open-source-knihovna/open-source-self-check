d35f85a6c8cc83d1dd5c16461bdd43c38123ccf6
Josef Moravec <josef.moravec@gmail.com>
2018-11-07 15:39:53 +0100

    Fix language updating

--- language.sh
+++ language.sh
@@ -3,11 +3,12 @@
 langs="en_GB cs_CZ"
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-cd $dir/..
+cd $dir
+
+tx pull
 
 for lang in $langs
 do
-    tx pull -l $lang
     xgettext --language=php --add-location --join-existing --output="$dir/module/Tohu/language/$lang.po" --from-code=UTF-8 --keyword=translate $(find "$dir/" -name '*.phtml') $(find "$dir/module/" -name '*.php')
     msgfmt "$dir/module/Tohu/language/$lang.po" -o "$dir/module/Tohu/language/$lang.mo"
 done;
